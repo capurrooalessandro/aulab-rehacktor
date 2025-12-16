39a7c838ae547e4666b5bb78ca0f25abb5662fd2
Redemption882 <loquendohyper@gmail.com>
2025-12-16 01:00:37 +0100

    fixed router for deploy

--- src/routing/router.jsx
+++ src/routing/router.jsx
@@ -15,59 +15,61 @@ import ProfileSettingsPage from "../views/auth/ProfileSettingsPage"
 import RegisterPage from "../views/auth/RegisterPage"
 import LoginPage from "../views/auth/LoginPage"
 
-const router = createBrowserRouter([
-    {
-        path: routes.home,
-        Component: Layout,
-        loader: getAllGenres,
-        children: [
-            {
-                path: routes.home,
-                Component: Home,
-                loader: getAllGamesLoader
-            },
-            {
-                path: routes.search,
-                Component: SearchPage,
-                loader: getSearchedGames
-            },
-            {
-                path: routes.genre,
-                Component: GenrePage,
-                loader: getSelectedGenre
-            }
-        ]
-    },
-    {
-        path: "/auth",
-        Component: AuthLayout,
-        children: [
-            {
-                path: routes.register,
-                Component: RegisterPage
-            },
-            {
-                path: routes.login,
-                Component: LoginPage
-            },
-            {
-                path: routes.profile,
-                Component: ProfilePage
-            },
-            {
-                path: routes.profileSettings,
-                Component: ProfileSettingsPage
-            }
-        ]
-    },
-    {
-        path: routes.detail,
-        Component: DetailPage,
-        loader: getGameDetails
-    },
+const router = createBrowserRouter(
+    [
+        {
+            path: routes.home,
+            Component: Layout,
+            loader: getAllGenres,
+            children: [
+                {
+                    path: routes.home,
+                    Component: Home,
+                    loader: getAllGamesLoader
+                },
+                {
+                    path: routes.search,
+                    Component: SearchPage,
+                    loader: getSearchedGames
+                },
+                {
+                    path: routes.genre,
+                    Component: GenrePage,
+                    loader: getSelectedGenre
+                }
+            ]
+        },
+        {
+            path: "/auth",
+            Component: AuthLayout,
+            children: [
+                {
+                    path: routes.register,
+                    Component: RegisterPage
+                },
+                {
+                    path: routes.login,
+                    Component: LoginPage
+                },
+                {
+                    path: routes.profile,
+                    Component: ProfilePage
+                },
+                {
+                    path: routes.profileSettings,
+                    Component: ProfileSettingsPage
+                }
+            ]
+        },
+        {
+            path: routes.detail,
+            Component: DetailPage,
+            loader: getGameDetails
+        }
+    ],
     {
         basename: import.meta.env.BASE_URL,
     }
-])
+)
 
 export default router;
